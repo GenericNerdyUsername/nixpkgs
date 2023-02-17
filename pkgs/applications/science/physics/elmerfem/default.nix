1fc4dbad832021b3c8fdf5e04b2e333300abdb26
GenericNerdyUsername <genericnerdyusername@proton.me>
2023-02-17 16:31:34 +0000

    elmerfem: switch from OCE to OCC

--- pkgs/applications/science/physics/elmerfem/default.nix
+++ pkgs/applications/science/physics/elmerfem/default.nix
@@ -1,4 +1,21 @@
-{ lib, stdenv, fetchFromGitHub, cmake, git, gfortran, mpi, blas, liblapack, pkg-config, libGL, libGLU, opencascade, libsForQt5, tbb, vtkWithQt5 }:
+{ lib
+, stdenv
+, fetchpatch
+, fetchFromGitHub
+, cmake
+, git
+, gfortran
+, mpi
+, blas
+, liblapack
+, pkg-config
+, libGL
+, libGLU
+, opencascade-occt
+, libsForQt5
+, tbb
+, vtkWithQt5
+}:
 
 stdenv.mkDerivation rec {
   pname = "elmerfem";
@@ -28,7 +45,7 @@ stdenv.mkDerivation rec {
     libsForQt5.qwt
     libGL
     libGLU
-    opencascade
+    opencascade-occt
     tbb
     vtkWithQt5
   ];
@@ -40,16 +57,17 @@ stdenv.mkDerivation rec {
   storepath = placeholder "out";
 
   cmakeFlags = [
-  "-DELMER_INSTALL_LIB_DIR=${storepath}/lib"
-  "-DWITH_OpenMP:BOOLEAN=TRUE"
-  "-DWITH_MPI:BOOLEAN=TRUE"
-  "-DWITH_QT5:BOOLEAN=TRUE"
-  "-DWITH_OCC:BOOLEAN=TRUE"
-  "-DWITH_VTK:BOOLEAN=TRUE"
-  "-DWITH_ELMERGUI:BOOLEAN=TRUE"
-  "-DCMAKE_INSTALL_LIBDIR=lib"
-  "-DCMAKE_INSTALL_INCLUDEDIR=include"
-  "-DCMAKE_OpenGL_GL_PREFERENCE=GLVND"
+    "-DELMER_INSTALL_LIB_DIR=${storepath}/lib"
+    "-DWITH_OpenMP:BOOLEAN=TRUE"
+    "-DWITH_MPI:BOOLEAN=TRUE"
+    "-DWITH_QT5:BOOLEAN=TRUE"
+    "-DWITH_OCC:BOOLEAN=TRUE"
+    "-DWITH_VTK:BOOLEAN=TRUE"
+    # Breaks compilation, uncomment at next release
+    #"-DWITH_ELMERGUI:BOOLEAN=TRUE"
+    "-DCMAKE_INSTALL_LIBDIR=lib"
+    "-DCMAKE_INSTALL_INCLUDEDIR=include"
+    "-DCMAKE_OpenGL_GL_PREFERENCE=GLVND"
   ];
 
   meta = with lib; {
