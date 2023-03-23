63a150cff11b594de1c83f8a261edc314e753ea3
GenericNerdyUsername <genericnerdyusername@proton.me>
2023-03-23 23:12:53 +0000

    python3Packages.cadquery: 0.18.2 -> 2.2.0

--- pkgs/development/python-modules/cadquery/default.nix
+++ pkgs/development/python-modules/cadquery/default.nix
@@ -1,103 +1,62 @@
 { lib
 , buildPythonPackage
-, toPythonModule
 , pythonOlder
 , pythonAtLeast
 , fetchFromGitHub
-, pyparsing
-, opencascade
+, cadquery-ocp
+, casadi-bindings
+, ezdxf
+, multimethod
+, nlopt-bindings
+, nptyping
+, path
+, setuptools-scm
+, typish
 , stdenv
-, python
-, cmake
-, swig
-, smesh
-, freetype
-, libGL
-, libGLU
-, libX11
-, six
-, pytest
-, makeFontsConf
-, freefont_ttf
 , Cocoa
 }:
 
-let
-  pythonocc-core-cadquery = toPythonModule (stdenv.mkDerivation {
-    pname = "pythonocc-core-cadquery";
-    version = "0.18.2";
+buildPythonPackage rec {
+  pname = "cadquery";
+  version = "2.2.0";
 
-    src = fetchFromGitHub {
-      owner = "CadQuery";
-      repo = "pythonocc-core";
-      # no proper release to to use, this commit copied from the Anaconda receipe
-      rev = "701e924ae40701cbe6f9992bcbdc2ef22aa9b5ab";
-      sha256 = "07zmiiw74dyj4v0ar5vqkvk30wzcpjjzbi04nsdk5mnlzslmyi6c";
-    };
+  src = fetchFromGitHub {
+    owner = "CadQuery";
+    repo = pname;
+    rev = version;
+    hash = "sha256-KzwPD+bhCHFiLVAVLn9cLsVke/N3PTh7d2pAzuPeHKI=";
+  };
 
-    nativeBuildInputs = [
-      cmake
-      swig
-    ];
+  # pythonRelaxDepsHook doesn't work bc it only modifies the dependencies of the wheel, not setup.py directly
+  postUnpack = ''
+    substituteInPlace source/setup.py \
+      --replace nptyping 'nptyping", #'
+  '';
 
-    buildInputs = [
-      python
-      opencascade
-      smesh
-      freetype
-      libGL
-      libGLU
-      libX11
-    ] ++ lib.optionals stdenv.isDarwin [ Cocoa ];
+  buildInputs = lib.optional stdenv.isDarwin [ Cocoa ];
 
-    propagatedBuildInputs = [
-      six
-    ];
+  propagatedBuildInputs = [
+    cadquery-ocp
+    casadi-bindings
+    ezdxf
+    multimethod
+    nlopt-bindings
+    nptyping
+    nptyping
+    path
+    typish
+  ];
 
-    cmakeFlags = [
-      "-Wno-dev"
-      "-DPYTHONOCC_INSTALL_DIRECTORY=${placeholder "out"}/${python.sitePackages}/OCC"
-      "-DSMESH_INCLUDE_PATH=${smesh}/include/smesh"
-      "-DSMESH_LIB_PATH=${smesh}/lib"
-      "-DPYTHONOCC_WRAP_SMESH=TRUE"
-    ];
-  });
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
 
-in
-  buildPythonPackage rec {
-    pname = "cadquery";
-    version = "2.0";
+  disabled = pythonOlder "3.8" || pythonAtLeast "3.11";
 
-    src = fetchFromGitHub {
-      owner = "CadQuery";
-      repo = pname;
-      rev = version;
-      sha256 = "1n63b6cjjrdwdfmwq0zx1xabjnhndk9mgfkm4w7z9ardcfpvg84l";
-    };
-
-    buildInputs = [
-      opencascade
-    ];
-
-    propagatedBuildInputs = [
-      pyparsing
-      pythonocc-core-cadquery
-    ];
-
-    FONTCONFIG_FILE = makeFontsConf {
-      fontDirectories = [ freefont_ttf ];
-    };
-
-    nativeCheckInputs = [
-      pytest
-    ];
-
-    disabled = pythonOlder "3.6" || pythonAtLeast "3.8";
-
-    meta = with lib; {
-      description = "Parametric scripting language for creating and traversing CAD models";
-      homepage = "https://github.com/CadQuery/cadquery";
-      license = licenses.asl20;
-      maintainers = with maintainers; [ marcus7070 ];
-    };
-  }
+  meta = with lib; {
+    description = "Parametric scripting language for creating and traversing CAD models";
+    homepage = "https://github.com/CadQuery/cadquery";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ marcus7070 ];
+  };
+}
